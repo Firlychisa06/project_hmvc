6488a84acdc1ee1d64d5a1e112b23f20020a2ba6
isnaini279 <isnaini2012@gmail.com>
2020-02-03 10:54:32 +0700

    add some example pages

--- application/modules/jabatan/controllers/Jabatan.php
+++ application/modules/jabatan/controllers/Jabatan.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Jabatan extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url', 'form');
+	}
+
 	public function index()
 	{
 		$this->load->view('starter');
--- application/modules/jabatan/views/starter.php
+++ application/modules/jabatan/views/starter.php
@@ -298,6 +298,41 @@ desired effect
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
+        <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Hover Data Table</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0
+                  </td>
+                  <td>Win 95+</td>
+                  <td> 4</td>
+                  <td>X</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+        </div>
+      </div>
+    </div>
 
     </section>
     <!-- /.content -->
@@ -405,5 +440,18 @@ desired effect
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
      user experience. -->
+<script>
+  $(function () {
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': true,
+      'searching'   : true,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+</script>
+
 </body>
 </html>
