b28c398a23d849e259358aa8ed928d14dac4e000
Firlychisa06 <@chisaputri06@gmail.com>
2020-02-11 17:12:47 +0700

    formlogin

--- application/modules/auth/views/login.php
+++ application/modules/auth/views/login.php
@@ -1,7 +1,31 @@
 <?php $this->load->view('header') ?>
 
-<!-- halaman login :
-- email
-- password -->
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Masukkan Email dan Password</p>
 
+    <!-- <form action="../../index2.html" method="post"> -->
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <a href="forget">Lupa Password</a>
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
 <?php $this->load->view('footer') ?>
