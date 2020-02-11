ca58fd179af3d0d61b1308f01fb29f969dd2bac3
Firlychisa06 <@chisaputri06@gmail.com>
2020-02-11 15:04:36 +0700

    formsurat

--- application/modules/manajemen_surat/views/footer.php
+++ application/modules/manajemen_surat/views/footer.php
@@ -102,6 +102,7 @@
 
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/dist/js/adminlte.min.js')?>"></script>
+<script src="<?= base_url('assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')?>"></script>
 
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
      Both of these plugins are recommended to enhance the
@@ -116,36 +117,20 @@
       'info'        : true,
       'autoWidth'   : false
     });
-  });
-  $(function () {
-    $('#datetimepicker1').datetimepicker();
-     $('#reservation').daterangepicker()
-    //Date range picker with time picker
-    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
-    //Date range as a button
-    $('#daterange-btn').daterangepicker(
-      {
-        ranges   : {
-          'Today'       : [moment(), moment()],
-          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
-          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-        },
-        startDate: moment().subtract(29, 'days'),
-        endDate  : moment()
-      },
-      function (start, end) {
-        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-      }
-    )
-
-    //Date picker
+        //Date picker
     $('#datepicker').datepicker({
       autoclose: true
     })
-  })
+  });
+  // $(function () {
+  //   $('#datetimepicker1').datetimepicker();
+
+
+
+    //Date picker
+    // $('#datepicker').datepicker({
+      // autoclose: true
+    // })
 </script>
 
 </body>
--- application/modules/manajemen_surat/views/form_surat.php
+++ application/modules/manajemen_surat/views/form_surat.php
@@ -34,7 +34,7 @@
                               <div class="input-group-addon">
                                 <i class="fa fa-calendar"></i>
                               </div>
-                              <input type="date" class="form-control pull-right" id="datepicker">
+                              <input type="text" class="form-control pull-right" id="datepicker">
                             </div>
                             </div>
                             </div>
