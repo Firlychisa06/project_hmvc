cc7637878493b3a0de9cc8780b3da9f30cdcd10b
nabilanajma <pnabila698@gmail.com>
2020-02-11 15:35:53 +0700

    list

--- application/modules/manajemen_surat/views/index.php
+++ application/modules/manajemen_surat/views/index.php
@@ -23,100 +23,89 @@
         <div class="row">
         	<div class="col-xs-12">
         		<div class="box">
-        			
-        			<!-- /.box-header -->
-        			<div class="box-body">
-                    <div class="dataTables_wrapper form-inline dt-bootstrap" id="example1_wrapper"></div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <!-- div class="dataTables_filter" id="example1_filter">
-                                <label>
-                                    Search:
-                                    <input class="form-control input-sm" aria-controls="example1" type="search" placeholder="">
-                                </label>
-                            </div> -->
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                        <table id="example1" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>No</th>
-                      <th>Asal surat</th>
-                      <th>Tanggal masuk</th>
-                      <th>Asal surat/instansi</th>
-                      <th>Tujuan surat</th>
-                      <th>Judul surat</th>
-                      <th>Aksi</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Semarang</td>
-                      <td>09 febuari 2020</td>
-                      <td>Polines Semarang</td>
-                      <td>Purwokerto</td>
-                      <td>Daftar Ulang Polines</td>
-                      <td> <button class="btn btn-warning" type="edit">Edit</button>
-                        <button class="btn btn-danger" type="delete">Delete</button>
-                      </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jakarta</td>
-                        <td>12 januari 2020</td>
-                        <td>Pos Indonesia</td>
-                        <td>Surabaya</td>
-                        <td>Panggilan kerja</td>
-                        <td><button class="btn btn-warning" type="edit">Edit</button>
-                        <button class="btn btn-danger" type="delete">Delete</button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Jogja</td>
-                        <td>12 maret 2019</td>
-                        <td>Kantor Pajak</td>
-                        <td>Jogja</td>
-                        <td>Tagihan Pajak</td>
-                        <td><button class="btn btn-warning" type="edit">Edit</button>
-                        <button class="btn btn-danger" type="delete">Delete</button></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Purwokerto</td>
-                        <td>25 januari 2020</td>
-                        <td>SMK Telkom Purwokerto</td>
-                        <td>Jogja</td>
-                        <td>Tagihan SPP</td>
-                        <td><button class="btn btn-warning" type="edit">Edit</button>
-                        <button class="btn btn-danger" type="delete">Delete</button></td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Jakarta</td>
-                        <td>10 november 2020</td>
-                        <td>PT KAI</td>
-                        <td>Malang</td>
-                        <td>Panggilan kerja</td>
-                        <td><button class="btn btn-warning" type="edit">Edit</button>
-                        <button class="btn btn-danger" type="delete">Delete</button></td>
-                    </tr>
-                  </tbody>
-              </table>
-                    </div>
-                    </div>
-                </div>
-        <!-- /.end-box-header -->
-                </div>
+            <div class="box-header">
+              <h3 class="box-title">Data Table With Full Features</h3>
             </div>
-        </div>
-<!-- /.end-row -->
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                                    <tr>
+                                      <th>No</th>
+                                      <th>Asal surat</th>
+                                      <th>Tanggal masuk</th>
+                                      <th>Asal surat/instansi</th>
+                                      <th>Tujuan surat</th>
+                                      <th>Judul surat</th>
+                                      <th>Aksi</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>1</td>
+                                  <td>Semarang</td>
+                                  <td>09 febuari 2020</td>
+                                  <td>Polines Semarang</td>
+                                  <td>Purwokerto</td>
+                                  <td>Daftar Ulang Polines</td>
+                                  <td> <button class="btn btn-warning" type="edit">Edit</button>
+                                    <button class="btn btn-danger" type="delete">Delete</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jakarta</td>
+                                <td>12 januari 2020</td>
+                                <td>Pos Indonesia</td>
+                                <td>Surabaya</td>
+                                <td>Panggilan kerja</td>
+                                <td><button class="btn btn-warning" type="edit">Edit</button>
+                                    <button class="btn btn-danger" type="delete">Delete</button></td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Jogja</td>
+                                    <td>12 maret 2019</td>
+                                    <td>Kantor Pajak</td>
+                                    <td>Jogja</td>
+                                    <td>Tagihan Pajak</td>
+                                    <td><button class="btn btn-warning" type="edit">Edit</button>
+                                        <button class="btn btn-danger" type="delete">Delete</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Purwokerto</td>
+                                        <td>25 januari 2020</td>
+                                        <td>SMK Telkom Purwokerto</td>
+                                        <td>Jogja</td>
+                                        <td>Tagihan SPP</td>
+                                        <td><button class="btn btn-warning" type="edit">Edit</button>
+                                            <button class="btn btn-danger" type="delete">Delete</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>5</td>
+                                            <td>Jakarta</td>
+                                            <td>10 november 2020</td>
+                                            <td>PT KAI</td>
+                                            <td>Malang</td>
+                                            <td>Panggilan kerja</td>
+                                            <td><button class="btn btn-warning" type="edit">Edit</button>
+                                                <button class="btn btn-danger" type="delete">Delete</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.end-box-header -->
+                    </div>
+                </div>
+            
+            <!-- /.end-row -->
 
-</section>
-<!-- /.content -->
-</div>
-<!-- /.content-wrapper -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 
-<?php $this->load->view('footer') ?>
+    <?php $this->load->view('footer') ?>
