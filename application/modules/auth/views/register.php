88464210e890cf47dd29acdfc54007d3aae24ba0
nabilanajma <pnabila698@gmail.com>
2020-02-11 17:18:40 +0700

    login

--- application/modules/auth/views/register.php
+++ application/modules/auth/views/register.php
@@ -1,5 +1,83 @@
 <?php $this->load->view('header') ?>
+<!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Please Register First</p>
 
+    <form action="assets/index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="Nama Lengkap" class="form-control" placeholder="Nama Lengkap">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+  </div>
+  	</form>
+
+    <form action="assets/index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="Email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+  </div>
+  	</form>
+
+    <form action="assets/index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="No Induk Pegawai" class="form-control" placeholder="No Induk Pegawai">
+        <span class="glyphicon glyphicon-address-card-o form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+      </div>
+  	</form>
+
+    <form action="assets/index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="Alamat" class="form-control" placeholder="Alamat">
+        <span class="glyphicon glyphicon-building form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+  </div>
+</form>
+
+	<!-- <form action="assets/index2.html" method="post"> -->
+      <div class="form-group has-feedback">
+        <input type="Password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+  </div>
+<!-- </form> -->
+
+		
+		<div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat"><i class="fa fa-send"></i>&nbsp; SIMPAN</button>
+        </div>
+        <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+          </div>
+      </div>
+  </div>
 <!-- halaman login :
 - nama lengkap
 - email
