a1a97ab5ed37c320a1f6a33b2c630562a3c53757
Firlychisa06 <@chisaputri06@gmail.com>
2020-02-12 14:55:01 +0700

    reset

--- application/modules/auth/views/reset_psswd.php
+++ application/modules/auth/views/reset_psswd.php
@@ -1,8 +1,30 @@
 <?php $this->load->view('header') ?>
 
-<!-- halaman login :
-- new password
-- retype password
--->
+<div class="login-box-body">
+    <p class="login-box-msg">Masukkan Password Baru</p>
+
+    <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="New Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+       <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Retype Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+<!--             <label>
+              <a href="forget">Forget Password</a>
+            </label> -->
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
 
 <?php $this->load->view('footer') ?>
