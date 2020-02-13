8d1d4afe75296d0a1980dc9db94b7f1f8d35375f
nabilanajma <pnabila698@gmail.com>
2020-02-13 17:38:03 +0700

    baru

--- application/modules/kepegawaian/views/form_pegawai.php
+++ application/modules/kepegawaian/views/form_pegawai.php
@@ -5,8 +5,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
-        <small>it all starts here</small>
+        Manajemen Kepegawaian
+        <!-- <small>it all starts here</small> -->
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -15,12 +15,51 @@
     </section>
 
     <!-- Main content -->
-    <section class="content">
+    <section class="content container-fluid">
+
+    <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="box">
+              <div class="box-header">
+                <h3 class="box-title">Input Data Pegawai</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                <form action="simpan" method="POST">
+                        <div class="form-group">
+                            <label>NAMA LENGKAP</label>
+                            <input type="text" name="nama_lengkap" class="form-control" placeholder="Masukkan Nama Lengkap" required>
+                        </div>
+                        <div class="form-group">
+                            <label>TEMPAT, TANGGAL LAHIR</label>
+                            <input type="text", type="date" name="ttl" class="form-control" placeholder="Masukkan Tempat, Tanggal Lahir" required>
+                        </div>
+                        <div class="form-group">
+                            <label>ALAMAT</label>
+                            <textarea class="textarea" placeholder=""
+                style="width: 100%; height: 100px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                        </div>
+                        <div class="form-group">
+                          <label>NO KTP</label>
+                          <input type="number" name="no_ktp" class="form-control" placeholder="Masukkan No Ktp" required>
+                        </div>
+                        <div class="form-group">
+                          <label>RIWAYAT PENDIDIKAN</label>
+                          <input type="text" name="riwayat_pendidikan" class="form-control" placeholder="SMP" required>
+                          <input type="text" name="riwayat_pendidikan" class="form-control" placeholder="SMA" required>
+                          <input type="text" name="riwayat_pendidikan" class="form-control" placeholder="PT" required>
+                        </div>
 
       <!-- Default box -->
+      <label>UPLOAD CV</label>
       <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+        <div class="box-header">
+          <h3 class="box-title">MASUKKAN CV
+            <!-- <small>Masukkan CV</small> -->
+          </h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
@@ -30,12 +69,71 @@
               <i class="fa fa-times"></i></button>
           </div>
         </div>
-        <div class="box-body">
-          Start creating your amazing application!
+
+        <div class="box-body pad">
+            <form>
+               <textarea class="textarea" placeholder=""
+                style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+            </form>
         </div>
+
+      <label>UPLOAD RIWAYAT HIDUP</label>
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">MASUKKAN RIWAYAT HIDUP
+            <!-- <small>Masukkan CV</small> -->
+          </h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+
+        <div class="box-body pad">
+            <form>
+               <textarea class="textarea" placeholder=""
+                style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+            </form>
+        </div>
+
+        <div class="container">
+          <br/>
+          <form name="" action="" method="POST">
+            <table>
+              <tr>
+                <th>Upload Sertifikat</th>
+              </tr>
+              <tbody id="dataTable" width="500px" border="1">              
+              <tr>
+                <td><input type="file" name="Sertifikat[]" id="name_1" placeholder="Sertifikat" class="form-control" /></td>
+              </tr>
+            </tbody>
+            <input type="hidden" name="count" id="count" value="1">
+              <tr><td colspan="5">&nbsp;</td></tr>
+            </table>
+            <button onclick="addRow('dataTable')" class="btn btn-primary">Add</button>
+            <button onclick="deleteRow('dataTable')" class="btn btn-danger">Delete</button>
+          </form>
+        </div>
+
+        <!-- nama lengkap pegawai
+             tempat tanggal lahir
+             alamat (text area)
+             no ktp
+             riwayat pendidikan (smp,sma,kuliah)
+             upload cv
+             upload riwayat hidup
+             upload sertifikat -->
+        <!-- <div class="box-body">
+          <table id="example1" class="table table-bordered table-striped" width="100%"></table>
+        </div> -->
         <!-- /.box-body -->
         <div class="box-footer">
-          Footer
+          <button type="submit" class="btn btn-primary">Submit</button>
         </div>
         <!-- /.box-footer-->
       </div>
--- application/modules/kepegawaian/views/index.php
+++ application/modules/kepegawaian/views/index.php
@@ -5,8 +5,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
-        <small>it all starts here</small>
+        Manajemen Kepegawaian
+        <!-- <small>it all starts here</small> -->
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -15,12 +15,85 @@
     </section>
 
     <!-- Main content -->
-    <section class="content">
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="box">
+              <div class="box-header">
+                <h3 class="box-title">Data Pegawai</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                <table id="example1" class="table table-bordered table-striped" width="100%">
+                  <thead>
+                    <tr>
+                      <th>Nama Lengkap</th>
+                      <th>Tempat, Tanggal Lahir</th>
+                      <th>Alamat</th>
+                      <th>No KTP</th>
+                      <th>Riwayat Pendidikan</th>
+                      <th>Aksi</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Muhammad Ilham</td>
+                      <td>Jakarta, 29 Maret 1995</td>
+                      <td>Jakarta Pusat</td>
+                      <td>330221234567789</td>
+                      <td>SMPN 18, SMK TELKOM, UGM</td>
+                      <td>
+                        <button class="btn btn-warning" type="edit">Edit</button>
+                        <button class="btn btn-danger" type="delete">Delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Jasmine Indah</td>
+                      <td>Purwokerto, 14 Oktober 1997</td>
+                      <td>Purwokerto</td>
+                      <td>330225467385989</td>
+                      <td>SMPN 5, SMAN 5, UGM</td>
+                      <td>
+                        <button class="btn btn-warning" type="edit">Edit</button>
+                        <button class="btn btn-danger" type="delete">Delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Lintang Muhammad</td>
+                      <td>Jakarta, 29 Maret 1995</td>
+                      <td>Jakarta Pusat</td>
+                      <td>33022584567899</td>
+                      <td>SMPN 18, SMK TELKOM, UGM</td>
+                      <td>
+                        <button class="btn btn-warning" type="edit">Edit</button>
+                        <button class="btn btn-danger" type="delete">Delete</button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Elang Muhammad Irawan</td>
+                      <td>Bekasi, 1 Januari 1994</td>
+                      <td>Bekasi</td>
+                      <td>3302243389399</td>
+                      <td>SMPN 28, SMAN 13, UI</td>
+                      <td>
+                        <button class="btn btn-warning" type="edit">Edit</button>
+                        <button class="btn btn-danger" type="delete">Delete</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
 
       <!-- Default box -->
-      <div class="box">
+      <!-- <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+          <h3 class="box-title">Data Pegawai</h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
@@ -29,7 +102,7 @@
             <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
               <i class="fa fa-times"></i></button>
           </div>
-        </div>
+        </div> -->
         <div class="box-body">
           Start creating your amazing application!
         </div>
@@ -47,3 +120,11 @@
   <!-- /.content-wrapper -->
 
 <?php $this->load->view('footer') ?>
+<!-- nama lengkap pegawai
+             tempat tanggal lahir
+             alamat (text area)
+             no ktp
+             riwayat pendidikan (smp,sma,kuliah)
+             upload cv
+             upload riwayat hidup
+             upload sertifikat -->
